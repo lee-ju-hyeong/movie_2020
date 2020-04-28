eef5e2e66dc10a6cd0678f4644864f508c86dd36
fred <fred.l@kakaocorp.com>
2020-04-28 10:14:19 +0900

    Updates

---
+++ precache-manifest.e8dad09f8a2944a68350626e550a2264.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b1c3f0748a4b94a83cc6ea3b07098c2",
+    "url": "/movie_2020/index.html"
+  },
+  {
+    "revision": "4fc15f6e0ac379cb13b2",
+    "url": "/movie_2020/static/css/main.6c5720cb.chunk.css"
+  },
+  {
+    "revision": "34901b2472fe4aafaf24",
+    "url": "/movie_2020/static/js/2.a3567de7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_2020/static/js/2.a3567de7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fc15f6e0ac379cb13b2",
+    "url": "/movie_2020/static/js/main.f2535d5f.chunk.js"
+  },
+  {
+    "revision": "682c1c85bafd0680e79e",
+    "url": "/movie_2020/static/js/runtime-main.8cc9e52a.js"
+  }
+]);
